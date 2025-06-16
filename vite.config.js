72c41ecc043c3d7657e78dfb177efb21fbe71870
chrelyonly <1172576293@qq.com>
2025-06-16 15:50:25 +0800

    1.更新脚手架

--- vite.config.js
+++ vite.config.js
@@ -3,35 +3,93 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-
-// https://vitejs.dev/config/
+import vueDevTools from 'vite-plugin-vue-devtools'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import setupExtend from 'vite-plugin-vue-setup-extend'
+// import compression from 'vite-plugin-compression'
+// https://vite.dev/config/
 export default defineConfig({
+
   plugins: [
     vue(),
     vueJsx(),
+    vueDevTools(),
+    AutoImport({
+      imports: [
+        'vue',
+        'vue-router',
+        'vuex'
+      ],
+      dts: false,
+      resolvers: [
+        ElementPlusResolver(),
+      ],
+    }),
+    Components({
+      resolvers: [
+        ElementPlusResolver(),
+      ],
+    }),
+    setupExtend(),
+    // compression()
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-// 代理配置
-  server: {
-    port: 5173,
-    // https: true,
-    https: {
-
-      cert: "./server.pem",
-      key: "./server.key",
     },
+  },
+  server: {
+    https: {
+      key: './server.key',
+      cert: './server.pem',
+    },
+    // port: 2888,
     proxy: {
       '/api/': {
-        // 代理到前端的地址的代理
-        target: "https://temp-img.chrelyonly.cn/api",
-        // 是否跨域
+        target: 'http://127.0.0.1:8077',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
+        ws: true,
+        rewrite: path => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+  esbuild: {
+    drop: ['console', 'debugger']
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name.endsWith('.css')) {
+            return 'css/[name]-[hash][extname]';
+          }
+          if (assetInfo.name.match(/\.(png|jpe?g|gif|svg|webp)$/)) {
+            return 'img/[name]-[hash][extname]';
+          }
+          return 'assets/[name]-[hash][extname]';
+        },
+        chunkFileNames: 'js/[name]-[hash].js',
+        entryFileNames: 'js/[name]-[hash].js',
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[2].split('/')[0].toString();
+          }
+        },
+      },
+    },
+    terserOptions: {
+      compress: {
+        // 开启压缩
+        drop_console: true, // 移除console
+        drop_debugger: true, // 移除debugger
+      },
+      sourceMap: false,
+      mangle: {
+        // 开启变量名混淆
+        toplevel: true,
+      },
+    },
   },
 })
